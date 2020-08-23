5682f903397099213a47823c13da7636a648835e
Nikita Peplov <npeplov@yandex.ru>
2020-08-23 19:10:22 +0300

    Added type check in calculateTotalMortgage

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,23 +1,34 @@
 "use strict"
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    let totalAmount;
 
-    let loan = amount - contribution;
+    let totalAmount; let inputTypeError = [];
 
-    if (date.getFullYear() + (new Date).getFullYear() == date.getMonth() + (new Date).getMonth()) 
-        totalAmount = 0;
+    !Number.isNaN(+percent)
+        ? percent = +percent
+        : inputTypeError = ['процент', percent];
     
+    !Number.isNaN(+contribution)
+        ? contribution = +contribution
+        : inputTypeError = ['1-й взнос', contribution];
+
+    !Number.isNaN(+amount)
+        ? amount = +amount
+        : inputTypeError = ['сумма кредита', amount];
+
+    if (inputTypeError.length != 0)
+        totalAmount = `Параметр ${inputTypeError[0]} содержит неправильное значение ${inputTypeError[1]}`;
+
     else {
+        let loan = amount - contribution;
         let months = (date.getFullYear() - (new Date).getFullYear()) * 12 + date.getMonth() - (new Date).getMonth();
         let p12 = percent/100 / 12;
         let payment = loan * ( p12 + percent/100 / 12/(( (1 + p12)**months ) - 1) )
-        totalAmount = (payment * months);
+        totalAmount = Math.round(payment * months* 100) / 100;
     }
 
-    return Math.round(totalAmount * 100) / 100;
+    return totalAmount;
 }
-
 function getGreeting(name) {
     if (
             name === null   // null
@@ -28,5 +39,7 @@ function getGreeting(name) {
     let greeting = `Привет, мир! Меня зовут ${name}`;
     return greeting;
 }
-console.log(calculateTotalMortgage(10,1000,50000, new Date(new Date().setFullYear(new Date().getFullYear() + 1))))
+
+console.log(calculateTotalMortgage("1",'1000','50000', new Date(new Date().setFullYear(new Date().getFullYear() + 1))))
+
 console.log(getGreeting(""))
