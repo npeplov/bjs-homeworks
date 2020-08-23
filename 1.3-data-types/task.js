ca680345dde32ff998bca1bca172ac0258239e83
Nikita Peplov <npeplov@yandex.ru>
2020-08-23 20:07:19 +0300

    Changed type check from if to map with loop

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,45 +1,42 @@
 "use strict"
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    let totalAmount; 
+    let inputTypeError = new Map([
+        [percent, "процент"],
+        [contribution, "1-й взнос"],
+        [amount, "сумма кредита"]
+    ]);
 
-    let totalAmount; let inputTypeError = [];
+    for (let key of inputTypeError.keys()) {
+        if (!Number.isNaN(+key))
+            inputTypeError.delete(key);
+        else {
+            totalAmount = `Параметр "${inputTypeError.get(key)}" содержит неправильное значение "${key}"`;
+        }
+    }
 
-    !Number.isNaN(+percent)
-        ? percent = +percent
-        : inputTypeError = ['процент', percent];
-    
-    !Number.isNaN(+contribution)
-        ? contribution = +contribution
-        : inputTypeError = ['1-й взнос', contribution];
-
-    !Number.isNaN(+amount)
-        ? amount = +amount
-        : inputTypeError = ['сумма кредита', amount];
-
-    if (inputTypeError.length != 0)
-        totalAmount = `Параметр ${inputTypeError[0]} содержит неправильное значение ${inputTypeError[1]}`;
-
-    else {
+    if (inputTypeError.size == 0) {
         let loan = amount - contribution;
         let months = (date.getFullYear() - (new Date).getFullYear()) * 12 + date.getMonth() - (new Date).getMonth();
         let p12 = percent/100 / 12;
-        let payment = loan * ( p12 + percent/100 / 12/(( (1 + p12)**months ) - 1) )
+        let payment = loan * ( p12 + percent/100 / 12/(( (1 + p12)**months ) - 1) );
         totalAmount = Math.round(payment * months* 100) / 100;
     }
-
     return totalAmount;
 }
+
 function getGreeting(name) {
     if (
             name === null   // null
         ||  !name           // undefined, ""
     )
-        name = "Аноним"
+        name = "Аноним";
 
     let greeting = `Привет, мир! Меня зовут ${name}`;
     return greeting;
 }
 
-console.log(calculateTotalMortgage("1",'1000','50000', new Date(new Date().setFullYear(new Date().getFullYear() + 1))))
+console.log(calculateTotalMortgage("1",1000,'50000', new Date(new Date().setFullYear(new Date().getFullYear() + 1))));
 
-console.log(getGreeting(""))
+console.log(getGreeting(""));
