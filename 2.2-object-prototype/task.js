bdfe7c6b994d0964330f35881fa27fa8ee47368e
Nikita Peplov <npeplov@yandex.ru>
2020-08-30 11:22:06 +0300

    Revert "Origin"
    This reverts commit 8b47cc435537776dc46aa4f97501c8bac58c2b06.

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,44 +1,11 @@
-// для задачи №1
-String.prototype.isPalindrome = function() {
-    // 1 создать копию строки в обр. порядке
-    let reversed = "";
-    // 1.1 цикл с последнего элемента
-    for (let i = this.length-1; i >= 0; i--){
-        reversed += this[i];
-    }
-    // 2. к нижнему регистру, убрать пробелы, сравнить
-    return reversed.toLowerCase().split(' ').join('') === this.toLowerCase().split(' ').join('');
-}
-// создаем строковый объект "А роза упала на лапу Азора" <= this
-console.log( "А роза упала на лапу Азора".isPalindrome() );
+//String.prototype.isPalindrome - для задачи №1
 
-// задача №2
 function getAverageMark(marks) {
-    if (marks.length === 0) {
-        return 0;
-    }
-
-    else {
-        let sum = 0;
-        for (let i=0; i < marks.length; i++) {
-            sum += marks[i];
-        }
-        const average = sum / marks.length;
-        const roundedAverage = Math.round(average);
-        return roundedAverage
-    }
+    // код для задачи №2 писать здесь
+    // return averageMark
 }
-console.log(getAverageMark([3,2,3,4,5]))
 
-// задача №3
-function checkBirthday(bdate) {
-    const now = new Date();
-
-    const birthday = new Date(bdate);
-
-    const diff = now - birthday;
-    const age = diff / 1000 / 60 / 60 / 24 / 365.25;
-
-    return age >= 18;
+function checkBirthday(birthday) {
+    // код для задачи №3 писать здесь
+    // return verdict
 }
-console.log(checkBirthday("1990-01-01"))
