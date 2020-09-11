cf28fc6bde1ad6e8dd04eec8b34350b34a8aeb47
Nikita Peplov <npeplov@yandex.ru>
2020-09-11 15:32:16 +0300

    memorize function is complete. compareArrays in process

--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -1,3 +1,4 @@
+"use strict"
 // Создайте функцию, которая сможет 
 // “запоминать” результаты другой функции,
 // которую ей передают в качестве аргумента.
@@ -10,53 +11,45 @@ function sleep(milliseconds)
 
 // 2.1.2. sum
 function sum(...args) {
-	// Замедление на половину секунды.
-	console.log('sleep 1000');
-	sleep(100);
-	return args.reduce((sum, arg) => {
-	  return sum += +arg;
-	}, 0);
+  console.log('sleep 500');
+	sleep(500);
+	return args.reduce((sum, arg) => sum += +arg, 0);
 }
 
 function compareArrays(arr1, arr2) {
-
-	function bigTen(elemArr1, index) {
-		return elemArr1 === arr2[index];
-	}
-
-	// без скобок bigTen - это указатель на описание функции
-	// т.е. вставится ее код, а не то что она возвращает
-	return arr1.every(bigTen);
+  return (arr1Elem, index) => {
+    console.log(arr1Elem);
+     arr1Elem === arr2[index];
+  }
+	// без скобок bigTen - это указатель на описание функции, вставится её код, а не return
 }
+
+console.log(compareArrays([1,2],[1,2])());
+
 
 function memorize(fn, limit) {
-	let memory = [];
-	
-	// memory.push({args, fn});
-	return (...args) => fn(...args);
+
+  const memory = [];
+
+  return (...args) => { // <- сюда передаст свои аргументы
+    const argument = [...args]; // <- fn, если выполнить
+
+    const matchIsFound = memory.find(
+      (element) => 
+        compareArrays(element.args, argument)
+    )
+
+    if (matchIsFound)
+      return matchIsFound.result;
+
+    memory.push({
+      args, result: fn(...args)
+    });
+
+    if (memory.length > limit)
+      memory.splice(0, 1);
+
+    return memory[memory.length - 1].result;
+  }
 }
 
-const mSum = memorize(sum, 5);
-// console.log(mSum(1,2));
-
-// mSum - функция memorize, которая вызывает функцию sum
-// как получить аргументы, переданные в mSum (1,2)?
-
-function createCounter(start) {
-	console.log('createCounter', start+1)
-	return function(){
-		console.log('function', start++);
-	}
-}
-
-let tick=createCounter(1),
-	tick10=createCounter(10);
-
-tick();
-tick();
-tick();
-tick10();
-tick10();
-tick10();
-tick10();
-tick10();
